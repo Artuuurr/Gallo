97a3ff732e1215a182201eef02231b58d6e7b224
Artur <shakirovartur60@gmail.com>
2024-09-13 15:08:44 +0500

    ...

--- src/components/form/formCSGO.jsx
+++ src/components/form/formCSGO.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
 
+import cs2Image from '../../../public/img/cs2.png'
+import warzoneImage from '../../../public/img/warzone.png'
+import dota2Image from '../../../public/img/dota2.png'
+import valorantImage from '../../../public/img/valorant.png'
+import apexImage from '../../../public/img/apex.png'
+import mobileLegendsImage from '../../../public/img/ML.png'
+
 function FormCSGO() {
-	// Список игр
+	// Список игр с изображениями
 	const games = [
-		{ name: 'CS 2', unit: 'Эло' },
-		{ name: 'War Zone', unit: 'Рейтинг' },
-		{ name: 'DOTA 2', unit: 'mmr' },
-		{ name: 'Valorant', unit: 'звание' },
-		{ name: 'Apex', unit: 'ранг' },
-		{ name: 'Mobile Legend', unit: 'звёзды' }, // Mobile Legends с системой звёзд
+		{ name: 'CS 2', unit: 'Эло', image: cs2Image },
+		{ name: 'War Zone', unit: 'Рейтинг', image: warzoneImage },
+		{ name: 'DOTA 2', unit: 'mmr', image: dota2Image },
+		{ name: 'Valorant', unit: 'звание', image: valorantImage },
+		{ name: 'Apex', unit: 'ранг', image: apexImage },
+		{ name: 'Mobile Legend', unit: 'звёзды', image: mobileLegendsImage },
 	]
 
+	// Ранги для игр Apex и Valorant
 	const apexRanks = [
 		'Бронза',
 		'Серебро',
@@ -20,7 +28,6 @@ function FormCSGO() {
 		'Мастер',
 		'Хищник',
 	]
-
 	const apexCharacters = [
 		'Wraith',
 		'Bloodhound',
@@ -29,14 +36,12 @@ function FormCSGO() {
 		'Pathfinder',
 		'Gibraltar',
 	]
-
 	const apexAchievements = [
 		'Убийца сезона',
 		'Легенда арены',
 		'Огненный путь',
 		'Штормовой охотник',
 	]
-
 	const valorantRanks = [
 		'Железо',
 		'Бронза',
@@ -109,176 +114,140 @@ function FormCSGO() {
 
 	return (
 		<div className='boost-calculator'>
-			<h2 className='boost-calculator__title'>Выберите игру и буст</h2>
-			<p className='boost-calculator__description'>
-				У каждой игры своя единица буста, в зависимости
-				<br /> от игры и количества единиц мы поможем вам
-				<br /> расчитать цену.
-			</p>
-			<div className='boost-calculator__game-selector'>
-				<button onClick={prevGame} className='boost-calculator__arrow-button'>
-					{'<'}
-				</button>
-				<span className='boost-calculator__game-name'>
-					{games[currentGameIndex].name}
-				</span>
-				<button onClick={nextGame} className='boost-calculator__arrow-button'>
-					{'>'}
-				</button>
-			</div>
-			<div className='boost-calculator__range-selector'>
-				{games[currentGameIndex].name === 'Apex' ? (
-					<>
-						<p className='boost-calculator__range-label'>Выбрать ранг:</p>
-						<div className='boost-calculator__slider-inputs'>
-							<select
-								name='from'
-								value={range.from}
-								onChange={handleInputChange}
-								className='boost-calculator__input'
-							>
-								{apexRanks.map((rank, index) => (
-									<option key={index} value={index}>
-										{rank}
-									</option>
-								))}
-							</select>
-							<select
-								name='to'
-								value={range.to}
-								onChange={handleInputChange}
-								className='boost-calculator__input'
-							>
-								{apexRanks.map((rank, index) => (
-									<option key={index} value={index}>
-										{rank}
-									</option>
-								))}
-							</select>
-						</div>
+			<div className='boost-calculator__left'>
+				<h2 className='boost-calculator__title'>Выберите игру и буст</h2>
+				<p className='boost-calculator__description'>
+					У каждой игры своя единица буста, в зависимости
+					<br /> от игры и количества единиц мы поможем вам
+					<br /> расчитать цену.
+				</p>
 
-						<p className='boost-calculator__range-label'>
-							Выбрать персонажа и ачивку:
-						</p>
-						<div className='boost-calculator__slider-inputs'>
-							<select
-								value={selectedCharacter}
-								onChange={handleCharacterChange}
-								className='boost-calculator__input'
-							>
-								{apexCharacters.map((character, index) => (
-									<option key={index} value={character}>
-										{character}
-									</option>
-								))}
-							</select>
-							<select
-								value={selectedAchievement}
-								onChange={handleAchievementChange}
-								className='boost-calculator__input'
-							>
-								{apexAchievements.map((achievement, index) => (
-									<option key={index} value={achievement}>
-										{achievement}
-									</option>
-								))}
-							</select>
-						</div>
-					</>
-				) : games[currentGameIndex].name === 'Valorant' ? (
-					<>
-						<p className='boost-calculator__range-label'>Выбрать звание:</p>
-						<div className='boost-calculator__slider-inputs'>
-							<select
-								name='from'
-								value={range.from}
-								onChange={handleInputChange}
-								className='boost-calculator__input'
-							>
-								{valorantRanks.map((rank, index) => (
-									<option key={index} value={index}>
-										{rank}
-									</option>
-								))}
-							</select>
-							<select
-								name='to'
-								value={range.to}
-								onChange={handleInputChange}
-								className='boost-calculator__input'
-							>
-								{valorantRanks.map((rank, index) => (
-									<option key={index} value={index}>
-										{rank}
-									</option>
-								))}
-							</select>
-						</div>
-					</>
-				) : games[currentGameIndex].name === 'Mobile Legend' ? (
-					<>
-						<p className='boost-calculator__range-label'>
-							Выбрать количество звёзд:
-						</p>
-						<div className='boost-calculator__slider-inputs'>
-							<input
-								type='number'
-								name='from'
-								value={range.from}
-								onChange={handleInputChange}
-								onFocus={handleFocus}
-								placeholder='От'
-								min='0'
-								max='100'
-								className='boost-calculator__input'
-							/>
-							<input
-								type='number'
-								name='to'
-								value={range.to}
-								onChange={handleInputChange}
-								onFocus={handleFocus}
-								placeholder='До'
-								min='0'
-								max='100'
-								className='boost-calculator__input'
-							/>
-						</div>
-						<input
-							type='range'
-							min='0'
-							max='100'
-							value={range[activeInput]}
-							onChange={handleRangeChange}
-							className='boost-calculator__range'
-						/>
-					</>
-				) : (
-					<>
-						<p className='boost-calculator__range-label'>
-							Выбрать кол-во по {games[currentGameIndex].unit}:
-						</p>
-						<div className='boost-calculator__slider-inputs'>
-							<input
-								type='number'
-								name='from'
-								value={range.from}
-								onChange={handleInputChange}
-								onFocus={handleFocus}
-								placeholder='От'
-								className='boost-calculator__input'
-							/>
-							<input
-								type='number'
-								name='to'
-								value={range.to}
-								onChange={handleInputChange}
-								onFocus={handleFocus}
-								placeholder='До'
-								className='boost-calculator__input'
-							/>
-						</div>
-						<div className='slider-wrapper'>
+				<div className='boost-calculator__game-selector'>
+					<button onClick={prevGame} className='boost-calculator__arrow-button'>
+						{'<'}
+					</button>
+					<span className='boost-calculator__game-name'>
+						{games[currentGameIndex].name}
+					</span>
+					<button onClick={nextGame} className='boost-calculator__arrow-button'>
+						{'>'}
+					</button>
+				</div>
+
+				<div className='boost-calculator__range-selector'>
+					{games[currentGameIndex].name === 'Apex' ? (
+						<>
+							<p className='boost-calculator__range-label'>Выбрать ранг:</p>
+							<div className='boost-calculator__slider-inputs'>
+								<select
+									name='from'
+									value={range.from}
+									onChange={handleInputChange}
+									className='boost-calculator__input'
+								>
+									{apexRanks.map((rank, index) => (
+										<option key={index} value={index}>
+											{rank}
+										</option>
+									))}
+								</select>
+								<select
+									name='to'
+									value={range.to}
+									onChange={handleInputChange}
+									className='boost-calculator__input'
+								>
+									{apexRanks.map((rank, index) => (
+										<option key={index} value={index}>
+											{rank}
+										</option>
+									))}
+								</select>
+							</div>
+
+							<p className='boost-calculator__range-label'>
+								Выбрать персонажа и ачивку:
+							</p>
+							<div className='boost-calculator__slider-inputs'>
+								<select
+									value={selectedCharacter}
+									onChange={handleCharacterChange}
+									className='boost-calculator__input'
+								>
+									{apexCharacters.map((character, index) => (
+										<option key={index} value={character}>
+											{character}
+										</option>
+									))}
+								</select>
+								<select
+									value={selectedAchievement}
+									onChange={handleAchievementChange}
+									className='boost-calculator__input'
+								>
+									{apexAchievements.map((achievement, index) => (
+										<option key={index} value={achievement}>
+											{achievement}
+										</option>
+									))}
+								</select>
+							</div>
+						</>
+					) : games[currentGameIndex].name === 'Valorant' ? (
+						<>
+							<p className='boost-calculator__range-label'>Выбрать звание:</p>
+							<div className='boost-calculator__slider-inputs'>
+								<select
+									name='from'
+									value={range.from}
+									onChange={handleInputChange}
+									className='boost-calculator__input'
+								>
+									{valorantRanks.map((rank, index) => (
+										<option key={index} value={index}>
+											{rank}
+										</option>
+									))}
+								</select>
+								<select
+									name='to'
+									value={range.to}
+									onChange={handleInputChange}
+									className='boost-calculator__input'
+								>
+									{valorantRanks.map((rank, index) => (
+										<option key={index} value={index}>
+											{rank}
+										</option>
+									))}
+								</select>
+							</div>
+						</>
+					) : (
+						<>
+							<p className='boost-calculator__range-label'>
+								Выбрать кол-во по {games[currentGameIndex].unit}:
+							</p>
+							<div className='boost-calculator__slider-inputs'>
+								<input
+									type='number'
+									name='from'
+									value={range.from}
+									onChange={handleInputChange}
+									onFocus={handleFocus}
+									placeholder='От'
+									className='boost-calculator__input'
+								/>
+								<input
+									type='number'
+									name='to'
+									value={range.to}
+									onChange={handleInputChange}
+									onFocus={handleFocus}
+									placeholder='До'
+									className='boost-calculator__input'
+								/>
+							</div>
 							<input
 								type='range'
 								min='0'
@@ -286,18 +255,30 @@ function FormCSGO() {
 								value={range[activeInput]}
 								onChange={handleRangeChange}
 								className='boost-calculator__range'
+								style={{
+									background: `linear-gradient(to right, red 0%, red ${range[activeInput]}%, gray ${range[activeInput]}%, gray 100%)`,
+								}}
 							/>
-						</div>
-					</>
-				)}
+						</>
+					)}
+					<div className='wrapBtn'>
+						<button
+							onClick={calculatePrice}
+							className='boost-calculator__calculate-button'
+						>
+							Рассчитать цену
+						</button>
+					</div>
+				</div>
 			</div>
-			<div className='wrapBtn'>
-				<button
-					onClick={calculatePrice}
-					className='boost-calculator__calculate-button'
-				>
-					Рассчитать цену
-				</button>
+
+			{/* Изображение игры, которое меняется */}
+			<div className='boost-calculator__right'>
+				<img
+					src={games[currentGameIndex].image}
+					alt={games[currentGameIndex].name}
+					className='boost-calculator__game-image'
+				/>
 			</div>
 		</div>
 	)
--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import FooterIcon from '../../../public/img/footerIcon.png'
 
 function Footer() {
-	return <div></div>
+	return (
+		<>
+			<footer>
+				<img src={FooterIcon} alt='Logo' />
+				<p>vk.com/purpur</p>
+			</footer>
+		</>
+	)
 }
 
 export default Footer
